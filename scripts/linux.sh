640dd912f3cc563c1a8ddcc2806d3ff6d68ad7d2
Dane Springmeyer <dane@mapbox.com>
2015-04-21 18:05:27 +0200

    try harder to get coverage running

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -3,11 +3,13 @@
 set -e -u
 set -o pipefail
 
-if [[ $CXX == "clang++" ]]; then
+if [[ ${CXX} =~ "clang" ]]; then
     make clean
     make coverage
     ./out/cov-test
     cp unit*gc* test/
     sudo pip install cpp-coveralls
     coveralls -i pbf.hpp --gcov-options '\-lp'
+else
+    echo "skipping building coverage"
 fi
