be31696ccdec86382e858ab7eff7491c4aac1775
Jochen Topf <jochen@topf.org>
2015-04-24 13:00:57 +0200

    Fix boolean test cases.

--- test/t/boolean/testcase.cpp
+++ test/t/boolean/testcase.cpp
@@ -5,16 +5,16 @@
 int main(int c, char *argv[]) {
     TestBoolean::Test msg;
 
-    msg.set_i(0);
+    msg.set_b(0);
     write_to_file(msg, "data-false.bin");
 
-    msg.set_i(1);
+    msg.set_b(1);
     write_to_file(msg, "data-true.bin");
 
-    msg.set_i(2);
+    msg.set_b(2);
     write_to_file(msg, "data-also-true.bin");
 
-    msg.set_i(127);
+    msg.set_b(127);
     write_to_file(msg, "data-still-true.bin");
 }
 
