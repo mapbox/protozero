6f12706cd9e5e0c31b3b24f9015a39107e110ca4
Jochen Topf <jochen@topf.org>
2015-05-06 18:45:02 +0200

    Improved travis build.

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -6,8 +6,8 @@ set -o pipefail
 if [[ ${CXX} =~ "clang" ]]; then
     make clean
     CXXFLAGS="--coverage" LDFLAGS="--coverage" make test
-    sudo pip install cpp-coveralls
-    coveralls -i pbf.hpp --gcov-options '\-lp'
+    pip install --user cpp-coveralls
+    ~/.local/bin/coveralls -i pbf.hpp --gcov-options '\-lp'
 else
     echo "skipping building coverage"
 fi
