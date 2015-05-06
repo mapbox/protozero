b396b71b0edfe6652d74b10cad785090b0de17c2
Jochen Topf <jochen@topf.org>
2015-05-06 19:44:49 +0200

    Disable -Wshadow for catch.hpp for GCC.

--- test/include/test.hpp
+++ test/include/test.hpp
@@ -1,5 +1,9 @@
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
 #include <catch.hpp>
+#pragma GCC diagnostic pop
+
 #include <pbf.hpp>
 
 extern std::string get_file_data(const std::string& filename);
