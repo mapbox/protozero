76586498f454c716c9d6fd38e1d7fea97e910ed9
Jochen Topf <jochen@topf.org>
2015-05-06 19:59:25 +0200

    Revert "Disable -Wshadow for catch.hpp for GCC."
    This reverts commit b396b71b0edfe6652d74b10cad785090b0de17c2.
    It leads to more warnings than before...

--- test/include/test.hpp
+++ test/include/test.hpp
@@ -1,9 +1,5 @@
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wshadow"
 #include <catch.hpp>
-#pragma GCC diagnostic pop
-
 #include <pbf.hpp>
 
 extern std::string get_file_data(const std::string& filename);
