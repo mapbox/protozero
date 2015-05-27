eb4bc7c92d1c6dbc3f76f516b6aa45b571fd2e1b
Jochen Topf <jochen@topf.org>
2015-05-27 15:16:47 +0200

    Fix exception tests.

--- test/t/exceptions/runtest.cpp
+++ test/t/exceptions/runtest.cpp
@@ -5,22 +5,22 @@ TEST_CASE("exceptions messages") {
 
     SECTION("pbf") {
         mapbox::util::pbf::exception e;
-        REQUIRE(e.what() == "pbf exception");
+        REQUIRE(std::string(e.what()) == std::string("pbf exception"));
     }
 
     SECTION("varint too long") {
         mapbox::util::pbf::varint_too_long_exception e;
-        REQUIRE(e.what() == "pbf varint too long exception");
+        REQUIRE(std::string(e.what()) == std::string("pbf varint too long exception"));
     }
 
     SECTION("unknown field type") {
         mapbox::util::pbf::unknown_field_type_exception e;
-        REQUIRE(e.what() == "pbf unknown field type exception");
+        REQUIRE(std::string(e.what()) == std::string("pbf unknown field type exception"));
     }
 
     SECTION("end of buffer") {
         mapbox::util::pbf::end_of_buffer_exception e;
-        REQUIRE(e.what() == "pbf end of buffer exception");
+        REQUIRE(std::string(e.what()) == std::string("pbf end of buffer exception"));
     }
 
 }
