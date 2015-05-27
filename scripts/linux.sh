c3025a431d586f1c46e9377ebda47a62ea477fb8
Jochen Topf <jochen@topf.org>
2015-05-27 14:50:11 +0200

    Fix coverage check.

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -7,7 +7,7 @@ if [[ ${CXX} =~ "clang" ]]; then
     make clean
     CXXFLAGS="--coverage" LDFLAGS="--coverage" make test
     pip install --user cpp-coveralls
-    ~/.local/bin/coveralls -i pbf.hpp --gcov-options '\-lp'
+    ~/.local/bin/coveralls -i pbf_common.hpp -i pbf_reader.hpp -i pbf_writer.hpp --gcov-options '\-lp'
 else
     echo "skipping building coverage"
 fi
