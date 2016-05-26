beded8d2c14cc5ce20188832f1bd75c52d26ce73
Jochen Topf <jochen@topf.org>
2016-05-26 20:56:10 +0200

    Add test for get_view().

--- test/t/string/test_cases.cpp
+++ test/t/string/test_cases.cpp
@@ -1,7 +1,7 @@
 
 #include <test.hpp>
 
-TEST_CASE("read string field") {
+TEST_CASE("read string field using get_string") {
 
     SECTION("empty") {
         const std::string buffer = load_data("string/data-empty");
@@ -40,6 +40,53 @@ TEST_CASE("read string field") {
             protozero::pbf_reader item(buffer.data(), i);
             REQUIRE(item.next());
             REQUIRE_THROWS_AS(item.get_string(), protozero::end_of_buffer_exception);
+        }
+    }
+
+}
+
+TEST_CASE("read string field using get_view") {
+
+    SECTION("empty") {
+        const std::string buffer = load_data("string/data-empty");
+
+        protozero::pbf_reader item(buffer);
+
+        REQUIRE(item.next());
+        auto v = item.get_view();
+        REQUIRE(v.size() == 0);
+        REQUIRE(!item.next());
+    }
+
+    SECTION("one") {
+        const std::string buffer = load_data("string/data-one");
+
+        protozero::pbf_reader item(buffer);
+
+        REQUIRE(item.next());
+        auto v = item.get_view();
+        REQUIRE(*v.data() == 'x');
+        REQUIRE(v.size() == 1);
+        REQUIRE(!item.next());
+    }
+
+    SECTION("string") {
+        const std::string buffer = load_data("string/data-string");
+
+        protozero::pbf_reader item(buffer);
+
+        REQUIRE(item.next());
+        REQUIRE(std::string(item.get_view()) == "foobar");
+        REQUIRE(!item.next());
+    }
+
+    SECTION("end_of_buffer") {
+        const std::string buffer = load_data("string/data-string");
+
+        for (std::string::size_type i = 1; i < buffer.size(); ++i) {
+            protozero::pbf_reader item(buffer.data(), i);
+            REQUIRE(item.next());
+            REQUIRE_THROWS_AS(item.get_view(), protozero::end_of_buffer_exception);
         }
     }
 
