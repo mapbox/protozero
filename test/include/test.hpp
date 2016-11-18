6c7d37cbefca9e53e75fe5b0bab7f829c0784dbd
Jochen Topf <jochen@topf.org>
2016-11-18 15:27:23 +0100

    Make assert_error constructor explicit.

--- test/include/test.hpp
+++ test/include/test.hpp
@@ -9,7 +9,7 @@
 // Define protozero_assert() to throw this error. This allows the tests to
 // check that the assert fails.
 struct assert_error : public std::runtime_error {
-    assert_error(const char* what_arg) : std::runtime_error(what_arg) {
+    explicit assert_error(const char* what_arg) : std::runtime_error(what_arg) {
     }
 };
 #define protozero_assert(x) if (!(x)) { throw(assert_error(#x)); }
