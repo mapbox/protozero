c9e9e860d4c407ace05d8ab29b35ad24120dcec5
Jochen Topf <jochen@topf.org>
2024-12-19 19:46:33 +0100

    Add explicit cast
    to fix implicit cast introduced in
    34d145521347f9bf18c86205d450d3bdc32ffb0a

--- include/protozero/buffer_string.hpp
+++ include/protozero/buffer_string.hpp
@@ -57,8 +57,8 @@ struct buffer_customization<std::string> {
         protozero_assert(from <= buffer->size());
         protozero_assert(to <= buffer->size());
         protozero_assert(from <= to);
-        buffer->erase(buffer->begin() + from,
-                      buffer->begin() + to);
+        buffer->erase(buffer->begin() + static_cast<std::string::difference_type>(from),
+                      buffer->begin() + static_cast<std::string::difference_type>(to));
     }
 
     static char* at_pos(std::string* buffer, std::size_t pos) {
