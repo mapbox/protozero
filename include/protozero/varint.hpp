93094298bc747c51f469a9c6cc907dce73ea6d46
Jochen Topf <jochen@topf.org>
2025-01-03 14:50:30 +0100

    Remove redundant inline specifiers
    Because constexpr functions are always inline.

--- include/protozero/varint.hpp
+++ include/protozero/varint.hpp
@@ -214,28 +214,28 @@ inline int length_of_varint(uint64_t value) noexcept {
 /**
  * ZigZag encodes a 32 bit integer.
  */
-inline constexpr uint32_t encode_zigzag32(int32_t value) noexcept {
+constexpr uint32_t encode_zigzag32(int32_t value) noexcept {
     return (static_cast<uint32_t>(value) << 1U) ^ static_cast<uint32_t>(-static_cast<int32_t>(static_cast<uint32_t>(value) >> 31U));
 }
 
 /**
  * ZigZag encodes a 64 bit integer.
  */
-inline constexpr uint64_t encode_zigzag64(int64_t value) noexcept {
+constexpr uint64_t encode_zigzag64(int64_t value) noexcept {
     return (static_cast<uint64_t>(value) << 1U) ^ static_cast<uint64_t>(-static_cast<int64_t>(static_cast<uint64_t>(value) >> 63U));
 }
 
 /**
  * Decodes a 32 bit ZigZag-encoded integer.
  */
-inline constexpr int32_t decode_zigzag32(uint32_t value) noexcept {
+constexpr int32_t decode_zigzag32(uint32_t value) noexcept {
     return static_cast<int32_t>((value >> 1U) ^ static_cast<uint32_t>(-static_cast<int32_t>(value & 1U)));
 }
 
 /**
  * Decodes a 64 bit ZigZag-encoded integer.
  */
-inline constexpr int64_t decode_zigzag64(uint64_t value) noexcept {
+constexpr int64_t decode_zigzag64(uint64_t value) noexcept {
     return static_cast<int64_t>((value >> 1U) ^ static_cast<uint64_t>(-static_cast<int64_t>(value & 1U)));
 }
 
