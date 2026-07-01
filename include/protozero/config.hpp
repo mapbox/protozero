2686f6510d32c636714d60e55e7f43205910c416
Konstantin Käfer <mail@kkaefer.com>
2026-07-01 11:08:25 +0200

    Harden byte order detection
    Use __BYTE_ORDER__ for GCC/Clang, and include Linux/BSD-specific headers for endian detection.
    
    Fixes #135.

--- include/protozero/config.hpp
+++ include/protozero/config.hpp
@@ -22,17 +22,48 @@ documentation.
 #define PROTOZERO_BIG_ENDIAN    4321
 
 // Find out which byte order the machine has.
-#if defined(__BYTE_ORDER)
-# if (__BYTE_ORDER == __LITTLE_ENDIAN)
+
+// __BYTE_ORDER__ is set by GCC and Clang.
+#if !defined(PROTOZERO_BYTE_ORDER) && defined(__BYTE_ORDER__)
+# if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #  define PROTOZERO_BYTE_ORDER PROTOZERO_LITTLE_ENDIAN
-# endif
-# if (__BYTE_ORDER == __BIG_ENDIAN)
+# elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 #  define PROTOZERO_BYTE_ORDER PROTOZERO_BIG_ENDIAN
 # endif
-#else
-// This probably isn't a very good default, but might do until we figure
-// out something better.
+#endif // defined(__BYTE_ORDER__)
+
+// On Linux, we can use endian.h.
+#if !defined(PROTOZERO_BYTE_ORDER) && defined(__linux__)
+# include <endian.h>
+# if defined(__BYTE_ORDER)
+#  if (__BYTE_ORDER == __LITTLE_ENDIAN)
+#   define PROTOZERO_BYTE_ORDER PROTOZERO_LITTLE_ENDIAN
+#  elif (__BYTE_ORDER == __BIG_ENDIAN)
+#   define PROTOZERO_BYTE_ORDER PROTOZERO_BIG_ENDIAN
+#  endif
+# endif
+#endif // !defined(PROTOZERO_BYTE_ORDER) && defined(__linux__)
+
+// On BSD, we can use <sys/endian.h>
+#if !defined(PROTOZERO_BYTE_ORDER) && \
+    (defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__))
+# include <sys/endian.h>
+# if defined(BYTE_ORDER)
+#  if (BYTE_ORDER == LITTLE_ENDIAN)
+#   define PROTOZERO_BYTE_ORDER PROTOZERO_LITTLE_ENDIAN
+#  elif (BYTE_ORDER == BIG_ENDIAN)
+#   define PROTOZERO_BYTE_ORDER PROTOZERO_BIG_ENDIAN
+#  endif
+# endif
+#endif
+
+// On Windows, we assume little endian.
+#if !defined(PROTOZERO_BYTE_ORDER) && defined(_MSC_VER)
 # define PROTOZERO_BYTE_ORDER PROTOZERO_LITTLE_ENDIAN
+#endif // !defined(PROTOZERO_BYTE_ORDER) && defined(_MSC_VER)
+
+#if !defined(PROTOZERO_BYTE_ORDER)
+# error "Could not determine byte order; define PROTOZERO_BYTE_ORDER"
 #endif
 
 // Check whether __builtin_bswap is available
